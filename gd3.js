86c369b43aa8c58eb52f00dc9fe5c90d09ef1143
connor <connor@cs.brown.edu>
2014-09-03 11:37:51 -0400

    Fix class typo

--- gd3.js
+++ gd3.js
@@ -101,7 +101,7 @@
         var aData = d.annotation, bbox = getScreenBBox();
         d3.selectAll(".gd3AnnotationViewDiv").remove();
         var container = d3.select(document.createElement("div"));
-        container.classed("class", "gd3AnnotationViewDiv");
+        container.attr("class", "gd3AnnotationViewDiv");
         container.style({
           background: "rgba(0,0,0,.75)",
           "border-radius": "3px",
