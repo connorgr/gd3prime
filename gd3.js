d2fd30ab72555d9c7366b1b81a83032f00ab3c91
connor <connor@cs.brown.edu>
2014-09-05 10:18:29 -0400

    Add on to magi parsing

--- gd3.js
+++ gd3.js
@@ -405,11 +405,44 @@
     return cnaChart(style);
   };
   function mutmtxData(inputData) {
-    var data = {};
+    var data = {
+      datasets: [],
+      labels: {
+        columns: {},
+        rows: {}
+      },
+      matrix: {}
+    };
+    data.get = function(attr) {
+      if (!attr) return null;
+      if (attr === "matrix") return data.matrix;
+      if (attr === "labels") return data.labels;
+    };
     function parseMagi() {
-      console.log(inputData);
+      function addDatasetsToMatrix() {
+        var matrix = data.matrix;
+        for (var rowKey in matrix) {
+          if (matrix.hasOwnProperty(rowKey)) {
+            for (var colKey in matrix[rowKey]) {
+              if (matrix[rowKey].hasOwnProperty(colKey)) {
+                matrix[rowKey][colKey].dataset = inputData.sampleToTypes[colKey];
+              }
+            }
+          }
+        }
+      }
+      data.matrix = inputData.M;
+      inputData.samples.forEach(function(s) {
+        columns[s._id] = s.name;
+      });
+      Object.keys(inputData.M).forEach(function(k, i) {
+        data.labels.rows[i] = k;
+      });
+      addDatasetsToMatrix();
     }
     parseMagi();
+    console.log(inputData);
+    console.log(data);
     return data;
   }
   function mutmtxChart(style) {
