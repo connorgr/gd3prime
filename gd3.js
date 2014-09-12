0fe5a7d0b0b791388fa7620bec7cb95a837bcf35
connor <connor@cs.brown.edu>
2014-09-12 16:03:07 -0400

    Remove row debugging log

--- gd3.js
+++ gd3.js
@@ -539,7 +539,6 @@
               };
             });
           }).enter().append("rect").attr("x", 0).attr("y", function(d) {
-            console.log(d.row, data.ids.rows, data.ids.rows.indexOf(d.row), style.rowHeight);
             return style.rowHeight * data.ids.rows.indexOf(d.row);
           }).attr("height", style.rowHeight).attr("width", colWidth).style("fill", function(d) {
             return colTypeToColor[d.type];
--- src/mutmtx/mutmtxChart.js
+++ src/mutmtx/mutmtxChart.js
@@ -167,7 +167,6 @@ function mutmtxChart(style) {
             .append('rect')
               .attr('x', 0)
               .attr('y', function(d) {
-                console.log(d.row, data.ids.rows, data.ids.rows.indexOf(d.row), style.rowHeight);
                 return style.rowHeight*data.ids.rows.indexOf(d.row);
               })
               .attr('height', style.rowHeight)
