2ed18edf54abec7a2da8e5dfeef7d3d6b9ff7246
connor <connor@cs.brown.edu>
2014-09-16 19:27:38 -0400

    Style cursor

--- gd3.js
+++ gd3.js
@@ -960,7 +960,7 @@
             return d.min;
           }).style({
             "box-shadow": "0px 0px 5px 0px rgba(0,0,0,0.75)",
-            cursor: "grab",
+            cursor: "pointer",
             fill: "url(#gradient)",
             stroke: "#666",
             "stroke-width": 1
