c1fa0dfe3388cf2340d66bdf61a4f8bb27c4dfd9
connor <connor@cs.brown.edu>
2014-12-17 14:47:02 -0500

    Add licensing information to tooltip library #2

--- src/tooltip/tooltip.js
+++ src/tooltip/tooltip.js
@@ -1,6 +1,21 @@
 import "tooltipStyle";
 import "tooltipView";
 
+////////////////////////////////////////////////////////////////////////////////
+// INFORMATION ABOUT GD3.TOOLTIP
+//
+// The gd3.tooltip() component is largely based on Justin Palmer's d3-tip
+// plug-in (https://github.com/Caged/d3-tip). Lots and lots of thanks, Justin!
+//
+// Differences:
+//   1. d3-tip is built such that only one tooltip can exist in a document,
+//        we altered this behavior, such that each call of gd3.tooltip() creates
+//        a brand new instance, meaning ∞ tooltips!
+//   2. to make making tooltips easier with complex data we've created a
+//        primitive type language for tooltip data that in turn lead to content
+//        factories that can automatically populate and format tooltips
+//
+
 gd3.tooltip = function(params) {
   var params = params || {},
       style  = tooltipStyle(params.style || {}),
